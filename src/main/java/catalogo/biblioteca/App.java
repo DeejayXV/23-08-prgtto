2e7087bf6877399cb2503cc3467f489ef6d72fd2
John <deejay.gvr@gmail.com>
2024-08-23 14:39:38 +0200

    classe Utente e Prestito

--- src/main/java/catalogo/biblioteca/App.java
+++ src/main/java/catalogo/biblioteca/App.java
@@ -3,16 +3,26 @@ package catalogo.biblioteca;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
 
 public class App {
 
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("bibliotecaPU");
         EntityManager em = emf.createEntityManager();
 
-        // Persistenza di un libro
         em.getTransaction().begin();
+
+        // Creazione di un utente
+        Utente utente = new Utente();
+        utente.setNome("Mario");
+        utente.setCognome("Rossi");
+        utente.setDataNascita(LocalDate.of(1985, 5, 15));
+        utente.setNumeroTessera("12345");
+
+        em.persist(utente);
+
+        // Creazione di un libro
         Libro libro = new Libro();
         libro.setCodiceIsbn("978-3-16-148410-0");
         libro.setTitolo("Il Signore degli Anelli");
@@ -22,26 +32,19 @@ public class App {
         libro.setGenere("Fantasy");
 
         em.persist(libro);
+
+        // Creazione di un prestito
+        Prestito prestito = new Prestito(utente, libro, LocalDate.now());
+        em.persist(prestito);
+
         em.getTransaction().commit();
 
-        // Persistenza di una rivista
-        em.getTransaction().begin();
-        Rivista rivista = new Rivista();
-        rivista.setCodiceIsbn("978-1-23-456789-0");
-        rivista.setTitolo("National Geographic");
-        rivista.setAnnoPubblicazione(2024);
-        rivista.setNumeroPagine(100);
-        rivista.setPeriodicita(Rivista.Periodicita.MENSILE);
-
-        em.persist(rivista);
-        em.getTransaction().commit();
-
-        // Recupero e stampa dei dati
-        Libro libroPersistito = em.find(Libro.class, "978-3-16-148410-0");
-        System.out.println("Libro recuperato: " + libroPersistito.getTitolo() + ", Autore: " + libroPersistito.getAutore() + ", Genere: " + libroPersistito.getGenere());
-
-        Rivista rivistaPersistita = em.find(Rivista.class, "978-1-23-456789-0");
-        System.out.println("Rivista recuperata: " + rivistaPersistita.getTitolo() + ", Periodicità: " + rivistaPersistita.getPeriodicita());
+        // Recupero e stampa dei dettagli del prestito
+        Prestito prestitoPersistito = em.find(Prestito.class, prestito.getId());
+        System.out.println("Prestito registrato: " + prestitoPersistito.getElementoPrestato().getTitolo() +
+                ", Utente: " + prestitoPersistito.getUtente().getNome() + " " +
+                prestitoPersistito.getUtente().getCognome() +
+                ", Data restituzione prevista: " + prestitoPersistito.getDataRestituzionePrevista());
 
         em.close();
         emf.close();
---
+++ src/main/java/catalogo/biblioteca/Prestito.java
@@ -0,0 +1,84 @@
+package catalogo.biblioteca;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Prestito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "utente_id")
+    private Utente utente;
+
+    @ManyToOne
+    @JoinColumn(name = "elemento_catalogo_id")
+    private ElementoCatalogo elementoPrestato;
+
+    private LocalDate dataInizioPrestito;
+
+    private LocalDate dataRestituzionePrevista;
+
+    private LocalDate dataRestituzioneEffettiva;
+
+    // Costruttore
+    public Prestito() {
+
+    }
+
+    public Prestito(Utente utente, ElementoCatalogo elementoPrestato, LocalDate dataInizioPrestito) {
+        this.utente = utente;
+        this.elementoPrestato = elementoPrestato;
+        this.dataInizioPrestito = dataInizioPrestito;
+        this.dataRestituzionePrevista = dataInizioPrestito.plusDays(30);
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    public ElementoCatalogo getElementoPrestato() {
+        return elementoPrestato;
+    }
+
+    public void setElementoPrestato(ElementoCatalogo elementoPrestato) {
+        this.elementoPrestato = elementoPrestato;
+    }
+
+    public LocalDate getDataInizioPrestito() {
+        return dataInizioPrestito;
+    }
+
+    public void setDataInizioPrestito(LocalDate dataInizioPrestito) {
+        this.dataInizioPrestito = dataInizioPrestito;
+        this.dataRestituzionePrevista = dataInizioPrestito.plusDays(30);
+    }
+
+    public LocalDate getDataRestituzionePrevista() {
+        return dataRestituzionePrevista;
+    }
+
+    public LocalDate getDataRestituzioneEffettiva() {
+        return dataRestituzioneEffettiva;
+    }
+
+    public void setDataRestituzioneEffettiva(LocalDate dataRestituzioneEffettiva) {
+        this.dataRestituzioneEffettiva = dataRestituzioneEffettiva;
+    }
+}
