584d68adcb28eeefc32049f5ad5cf6d55005d7be
John <deejay.gvr@gmail.com>
2024-08-23 12:45:30 +0200

    Initial commit

---
+++ src/main/java/catalogo/biblioteca/ElementoCatalogo.java
@@ -0,0 +1,51 @@
+package catalogo.biblioteca;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_elemento")
+public abstract class ElementoCatalogo {
+
+    @Id
+    private String codiceIsbn;
+
+    private String titolo;
+
+    private int annoPubblicazione;
+
+    private int numeroPagine;
+
+    // Getters e Setters
+    public String getCodiceIsbn() {
+        return codiceIsbn;
+    }
+
+    public void setCodiceIsbn(String codiceIsbn) {
+        this.codiceIsbn = codiceIsbn;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(int annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+}
